c6efccf25cc4246786719983c3d231fe20032cfb
oussib <oussib@hotmail.fr>
2025-02-27 14:20:12 +0100

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "results",       // Dossier où seront stockés les résultats
+      overwrite: false,           // Ne pas écraser les rapports précédents
+      html: false,                // Ne pas générer de fichier HTML
+      json: true                  // Générer un fichier JSON
+    }
+  }
+});
